42dbcb2ec2e2c407b521b33a9594c1ba3186ed56
blackdanamite <seagrendaniel@gmail.com>
2019-10-08 17:21:03 -0700

    add more styling

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -8,20 +8,38 @@ const AboutPage = (props) => {
     return (
         <div>
             <NavBar />
-            <h1 className="about-header fade-in">About S & S Farms</h1>
-            <div className="about-content">
-            <img src="https://i.imgur.com/ri62OY7.jpg" alt="Zeldas-logo" className="zelda-logo"/>
-            <p>
-                In 2016 Lou Savage and Cheryl Seagren returned to Oregon from North Africa with Zelda, their Pembrooke Corgi, to establish S&S Farms. 
-                They wanted to have a place where they could explore their love of the outdoors, grow their own food and living in the heart of Pinot country wasn't a bad perk. 
-                Over time they realized they wanted to share the fruits of their labors with others, and thus, <strong>Zelda's</strong> was born. 
-                Zelda's all natural berry jams, preserves, and fruit sauces are made from marionberries, raspberries, blueberries, grapes, figs, and blackberries grown in 
-                some of the richest soil in the world. 
+            <div className="fade-in">
+                <h1 className="about-header">About S & S Farms</h1>
+                <div className="about-content">
+                    <img src="https://i.imgur.com/ri62OY7.jpg" alt="Zeldas-logo" className="zelda-logo" />
+                    <div>
+                        <p>
+                            In 2016 Lou Savage and Cheryl Seagren returned to Oregon from North Africa with Zelda, their Pembrooke Corgi, to establish S&S Farms.
+                            They wanted to have a place where they could explore their love of the outdoors, grow their own food and living in the heart of Pinot country wasn't a bad perk.
+                Over time they realized they wanted to share the fruits of their labors with others, and thus, <strong>Zelda's</strong> was born.
+                                    Zelda's all natural berry jams, preserves, and fruit sauces are made from marionberries, raspberries, blueberries, grapes, figs, and blackberries grown in
+                                    some of the richest soil in the world.
                 <br></br>
+                            <br></br>
+                            Zelda is the guardian of S&S Farms and watches over the goats, chickens, pigs, and cows that share the farm with her.
+                            They are committed to sustainable farming, concentrating on growing organic berries, grapes, figs, apples, and pears for Zelda’s products.
                 <br></br>
-                Zelda is the guardian of S&S Farms and watches over the goats, chickens, pigs, and cows that share the farm with her.
-                They are committed to sustainable farming, concentrating on growing organic berries, grapes, figs, apples, and pears for Zelda’s products.
+                            <br></br>
+                            Some of Zelda's products include:
             </p>
+                        <ul className="product-list">
+                            <li>Marionberry Jam</li>
+                            <li>Plum Jam</li>
+                            <li>Blackberry Jam</li>
+                            <li>Fig Preserves & Marmalade</li>
+                            <li>Concord Grape Jam</li>
+                            <li>Apple Sauce</li>
+                            <li>Pear Sauce</li>
+                            <li>Berry Chocolate Topping</li>
+
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import NavBar from '../../components/NavBar/NavBar';
 
 import './HomePage.css'
 
 const HomePage = (props) => {
-    return(
+    return (
         <div>
-            <NavBar />
-            <div className="homepage">
-            <h1 className="home-header fade-in">WELCOME TO S & S FARMS</h1>
-            <div className="container-fluid intro">
-            <img src="https://i.imgur.com/uNvJrII.jpg" alt="fall colors" className="fall-field"/>
-            <p className="container ">
-            Oregon's beautiful Willamette Valley is home to S&S Farms, an organic farm that produces marionberries, raspberries, blueberries, grapes, figs and blackberries. 
-            The Farm offers Zelda’s Hideaway, a cozy overnight spot that is a perfect location for visiting wine country, Portland, or the Oregon Coast. 
-            </p>
-            </div>
+            <div className="nav-bar"><NavBar /></div>
+            <div className="homepage fade-in">
+                <h1 className="home-header">WELCOME TO S & S FARMS</h1>
+                <div className="fluid-container intro">
+                    <div className="row">
+                        <div className="col-sm">
+
+                        </div>
+                        <p className="col-sm intro-text">
+                            Oregon's beautiful Willamette Valley is home to S&S Farms, an organic farm that produces marionberries, raspberries, blueberries, grapes, figs and blackberries.
+                            The Farm offers Zelda’s Hideaway, a cozy overnight spot that is a perfect location for visiting wine country, Portland, or the Oregon Coast.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     )
