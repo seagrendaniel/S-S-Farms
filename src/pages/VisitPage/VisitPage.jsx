d61f95931ecdc522cf3c8f636726f529c5ac0cf8
blackdanamite <seagrendaniel562@gmail.com>
2019-11-13 10:56:02 -0800

    visit page descriptions added

--- src/pages/VisitPage/VisitPage.jsx
+++ src/pages/VisitPage/VisitPage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import NavBar from '../../components/NavBar/NavBar';
+
 import './VisitPage.css';
 
 const VisitPage = (props) => {
@@ -15,7 +16,7 @@ const VisitPage = (props) => {
             <div className="fade-in">
                 <h1 className="visit-header"> Visit Zelda's Retreat</h1>
                 <p className="visit-info">
-                    Zelda's retreat features a queen size bed.
+                    Zelda's Retreat is a large private, self-contained suite attached attached to the main farmhouse. It overlooks the surrounding fields and farms and is close to over 100 wineries. The space offers a private entrance, use of a lovely patio, and parking. The suite is decorated in the traditional farmhouse style and features family antiques. Coffee and a delicious light breakfast accompanied by Zelda's jam is offered as well. 
                 </p>
             </div>
         </div>
